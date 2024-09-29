7d5df392d1425b643f52213439b87aa2ed9f495f
ttomas0520 <79412823+ttomas0520@users.noreply.github.com>
2024-09-29 19:03:40 +0200

    regen UI api client

--- UI/AnimalAidPlatform/src/apiClient/data-contracts.ts
+++ UI/AnimalAidPlatform/src/apiClient/data-contracts.ts
@@ -110,6 +110,45 @@ export interface CategoryRequestDto {
   urlhandle?: string | null;
 }
 
+export interface Coordinate {
+  /** @format double */
+  x?: number;
+  /** @format double */
+  y?: number;
+  /** @format double */
+  z?: number;
+  /** @format double */
+  m?: number;
+  coordinateValue?: Coordinate;
+  isValid?: boolean;
+}
+
+export type CoordinateEqualityComparer = object;
+
+export interface CoordinateSequence {
+  /** @format int32 */
+  dimension?: number;
+  /** @format int32 */
+  measures?: number;
+  /** @format int32 */
+  spatial?: number;
+  ordinates?: Ordinates;
+  hasZ?: boolean;
+  hasM?: boolean;
+  /** @format int32 */
+  zOrdinateIndex?: number;
+  /** @format int32 */
+  mOrdinateIndex?: number;
+  first?: Coordinate;
+  last?: Coordinate;
+  /** @format int32 */
+  count?: number;
+}
+
+export interface CoordinateSequenceFactory {
+  ordinates?: Ordinates;
+}
+
 export interface CreatePostDTO {
   /** @minLength 1 */
   title: string;
@@ -125,6 +164,42 @@ export interface CreatePostDTO {
 export interface CreateRoleDTO {
   /** @minLength 1 */
   roleName: string;
+}
+
+/** @format int32 */
+export enum Dimension {
+  Value0 = 0,
+  Value1 = 1,
+  Value2 = 2,
+  Value3 = 3,
+  Value3 = -3,
+  Value2 = -2,
+  Value1 = -1,
+}
+
+export interface Envelope {
+  isNull?: boolean;
+  /** @format double */
+  width?: number;
+  /** @format double */
+  height?: number;
+  /** @format double */
+  diameter?: number;
+  /** @format double */
+  minX?: number;
+  /** @format double */
+  maxX?: number;
+  /** @format double */
+  minY?: number;
+  /** @format double */
+  maxY?: number;
+  /** @format double */
+  area?: number;
+  /** @format double */
+  minExtent?: number;
+  /** @format double */
+  maxExtent?: number;
+  centre?: Coordinate;
 }
 
 export interface FeedPost {
@@ -144,6 +219,7 @@ export interface FeedPost {
   /** @format double */
   geoLat?: number;
   address?: string | null;
+  location?: Point;
   /** @format date-time */
   postDate?: string;
   creatorId?: string | null;
@@ -162,6 +238,48 @@ export interface FeedPostResponseDTO {
   creatorName?: string | null;
   imageUrl?: string | null;
 }
+
+export interface Geometry {
+  factory?: GeometryFactory;
+  userData?: any;
+  /** @format int32 */
+  srid?: number;
+  geometryType?: string | null;
+  ogcGeometryType?: OgcGeometryType;
+  precisionModel?: PrecisionModel;
+  coordinate?: Coordinate;
+  coordinates?: Coordinate[] | null;
+  /** @format int32 */
+  numPoints?: number;
+  /** @format int32 */
+  numGeometries?: number;
+  isSimple?: boolean;
+  isValid?: boolean;
+  isEmpty?: boolean;
+  /** @format double */
+  area?: number;
+  /** @format double */
+  length?: number;
+  centroid?: Point;
+  interiorPoint?: Point;
+  pointOnSurface?: Point;
+  dimension?: Dimension;
+  boundary?: Geometry;
+  boundaryDimension?: Dimension;
+  envelope?: Geometry;
+  envelopeInternal?: Envelope;
+  isRectangle?: boolean;
+}
+
+export interface GeometryFactory {
+  precisionModel?: PrecisionModel;
+  coordinateSequenceFactory?: CoordinateSequenceFactory;
+  /** @format int32 */
+  srid?: number;
+  geometryServices?: NtsGeometryServices;
+}
+
+export type GeometryOverlay = object;
 
 export interface LocationDTO {
   /** @format double */
@@ -189,6 +307,7 @@ export interface NotificationSettings {
   /** @format double */
   geoLat?: number;
   address?: string | null;
+  location?: Point;
   /** @format double */
   radius?: number;
   categories?: Category[] | null;
@@ -200,6 +319,138 @@ export interface NotificationSettingsDto {
   /** @format double */
   radius?: number;
   categoryIds?: number[] | null;
+}
+
+export interface NtsGeometryServices {
+  geometryOverlay?: GeometryOverlay;
+  coordinateEqualityComparer?: CoordinateEqualityComparer;
+  /** @format int32 */
+  defaultSRID?: number;
+  defaultCoordinateSequenceFactory?: CoordinateSequenceFactory;
+  defaultPrecisionModel?: PrecisionModel;
+}
+
+/** @format int32 */
+export enum OgcGeometryType {
+  Value1 = 1,
+  Value2 = 2,
+  Value3 = 3,
+  Value4 = 4,
+  Value5 = 5,
+  Value6 = 6,
+  Value7 = 7,
+  Value8 = 8,
+  Value9 = 9,
+  Value10 = 10,
+  Value11 = 11,
+  Value12 = 12,
+  Value13 = 13,
+  Value14 = 14,
+  Value15 = 15,
+  Value16 = 16,
+}
+
+/** @format int32 */
+export enum Ordinates {
+  Value0 = 0,
+  Value1 = 1,
+  Value2 = 2,
+  Value3 = 3,
+  Value4 = 4,
+  Value7 = 7,
+  Value8 = 8,
+  Value16 = 16,
+  Value32 = 32,
+  Value64 = 64,
+  Value128 = 128,
+  Value256 = 256,
+  Value512 = 512,
+  Value1024 = 1024,
+  Value2048 = 2048,
+  Value4096 = 4096,
+  Value8192 = 8192,
+  Value16384 = 16384,
+  Value32768 = 32768,
+  Value65535 = 65535,
+  Value65536 = 65536,
+  Value65539 = 65539,
+  Value65543 = 65543,
+  Value131072 = 131072,
+  Value262144 = 262144,
+  Value524288 = 524288,
+  Value1048576 = 1048576,
+  Value2097152 = 2097152,
+  Value4194304 = 4194304,
+  Value8388608 = 8388608,
+  Value16777216 = 16777216,
+  Value33554432 = 33554432,
+  Value67108864 = 67108864,
+  Value134217728 = 134217728,
+  Value268435456 = 268435456,
+  Value536870912 = 536870912,
+  Value1073741824 = 1073741824,
+  Value2147483648 = -2147483648,
+  Value65536 = -65536,
+  Value1 = -1,
+}
+
+export interface Point {
+  factory?: GeometryFactory;
+  userData?: any;
+  /** @format int32 */
+  srid?: number;
+  precisionModel?: PrecisionModel;
+  /** @format int32 */
+  numGeometries?: number;
+  isSimple?: boolean;
+  isValid?: boolean;
+  /** @format double */
+  area?: number;
+  /** @format double */
+  length?: number;
+  centroid?: Point;
+  interiorPoint?: Point;
+  pointOnSurface?: Point;
+  envelope?: Geometry;
+  envelopeInternal?: Envelope;
+  isRectangle?: boolean;
+  coordinateSequence?: CoordinateSequence;
+  coordinates?: Coordinate[] | null;
+  /** @format int32 */
+  numPoints?: number;
+  isEmpty?: boolean;
+  dimension?: Dimension;
+  boundaryDimension?: Dimension;
+  /** @format double */
+  x?: number;
+  /** @format double */
+  y?: number;
+  coordinate?: Coordinate;
+  geometryType?: string | null;
+  ogcGeometryType?: OgcGeometryType;
+  boundary?: Geometry;
+  /** @format double */
+  z?: number;
+  /** @format double */
+  m?: number;
+}
+
+export interface PrecisionModel {
+  isFloating?: boolean;
+  /** @format int32 */
+  maximumSignificantDigits?: number;
+  /** @format double */
+  scale?: number;
+  /** @format double */
+  gridSize?: number;
+  precisionModelType?: PrecisionModels;
+}
+
+/** @format int32 */
+export enum PrecisionModels {
+  Value0 = 0,
+  Value1 = 1,
+  Value2 = 2,
 }
 
 export interface RegisterDTO {
